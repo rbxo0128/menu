c1ed8340e6997dce153902a18e64a8f6f5194f90
rbxo0128 <rbxo0128@naver.com>
2025-03-18 20:48:19 +0900

    fix

--- src/main/java/CommandRegistrar.java
+++ src/main/java/CommandRegistrar.java
@@ -1,10 +1,12 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
 public class CommandRegistrar {
     public static void registerCommands(JDA jda) {
-        String guildId = "912648428763349023";
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        String guildId = dotenv.get("GUILD_ID");
         jda.getGuildById(guildId).updateCommands().addCommands(
                 Commands.slash("메뉴추천", "오늘 먹을 메뉴를 추천해줍니다.")
                         .addOption(OptionType.STRING, "meal", "아침, 점심, 저녁", true)
